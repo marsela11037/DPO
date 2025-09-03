09aff8ae4bba61b6a26a0b0bf4b48083cfa12cf3
марсела <albedo_albedo@vk.com>
2025-09-03 12:37:04 +0300

    изменение файла sprint.py
    прописаны строковые литералы с рекомендациями по оптимизации и рефакторингу программного кода, а так жн для создания обёртки(wrapper) для унаследованного проекта

--- sprint.py
+++ sprint.py
@@ -1,60 +1,52 @@
 import turtle
 import random
-import time
 
-#глобальные переменные (не принятая практика с точки зрения организации функций)
+# Глобальные переменные
+'''! Использование глобальных переменных в функциях '''
 sc = None
-t1 = None # объект библиотеки Turtle (черепашка) красного цвета 
-t2 = None # объект библиотеки Turtle (черепашка) красного цвета
+t1 = None
+t2 = None
 win = False
 
-# функция 
+# Функция базовой настройки
+''' Логика настроек программы '''
 def setup():
     global sc, t1, t2
     sc = turtle.Screen()
     sc.setup(500,500)
-    t1 = turtle.Turle()
-    t2 = turtle.Turle()
-    t1.color('ped')
-    t2.color('green')
+    t1 = turtle.Turtle()
+    t2 = turtle.Turtle()
+    t1.color('red')
+    t2.color('blue')
     t1.shape('turtle')
     t2.shape('turtle')
     t1.penup()
-    t2.penup()
+    t1.penup()
     t1.goto(-150,50)
     t2.goto(-150,-50)
-    
-    #функция для запуска игры 
-    def start_race():
-        global win
-        for i in range(100):
-            t1.forward(random.randint(1,5))
-            t2.forward(random.randint(1,5))
-
-
-
-
-
-#функция для определения победителя 
-def check_winner():
+# Функция для запуска игры
+''' Отсутствие структуры (весь алгоритм написан в функциях, а не классах) '''
+def start_race():
     global win 
-    if t1.xcor() > 150:
-        print('победила красная черепашка')
+    for i in range(100):
+        if not win:
+            t1.forward(random.randint)
+            t1.forward(random.randint)
+            check_winner()
+        else:
+            break
+
+def check_winner():
+    global win
+    if t1.xcor()>150:
+        print('Победила красная черепашка')
         win = True
-    if t2.xcor() > 150:
-        print('победила зеленая черепашка')
+    if t2.xcor()>150:
+        print('Победила зелёная черепашка')
         win = True
 
-#код запуска
+'''Логика игрового процесса'''
 setup()
 start_race()
 
-
-'''проблемаика данного кода:
-1. использлование глобальных переменных в функциях.
-2. смешение логики и представления (логика настроек программы 
-и логика гонки еаходятся в одном месте). 
-3. отсуствие структуры (весь алгоритм написан в фунциях, а не класах).
-
-4. код невозможно переиспользовать, поскольку он не представляет собой модуль'''
-
+'''код невозможно переиспользовать, поскольку он не представляет собой модуль.'''
